60a001f303a492fe3142452a3c2a26f7f95e6ea2
ZhongWen Li <lizhongwen1989@gmail.com>
2016-10-18 21:09:46 +0800

    Update: Add TestCase

---
+++ src/test/java/test/HelloTestCase.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+public class HelloTestCase {
+
+	@Test
+	public void test() {
+		System.out.println(new Date());
+	}
+
+	@Test
+	public void test1() {
+		System.out.println(new Date());
+	}
+
+	@Test
+	public void test2() {
+		System.out.println(new Date());
+	}
+	
+}
---
+++ src/test/java/test/WorldTestCase.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.time.LocalTime;
+
+import org.junit.Test;
+
+public class WorldTestCase {
+
+	@Test
+	public void test() {
+		System.out.println(LocalTime.now());
+	}
+
+	@Test
+	public void test1() {
+		System.out.println(LocalTime.now());
+	}
+
+	@Test
+	public void test2() {
+		System.out.println(LocalTime.now());
+	}
+	
+}
